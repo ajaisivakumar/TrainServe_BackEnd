3016c4578c8a6c3ce53e48f419c9915e61a06564
ajaisivakumar <ajaipc405@gmail.com>
2026-06-26 22:16:49 +0530

    Change SSL rejectUnauthorized to true

--- src/db/pool.js
+++ src/db/pool.js
@@ -10,7 +10,7 @@ const pool = new Pool({
   user:     process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   ssl: process.env.DB_HOST && process.env.DB_HOST !== 'localhost'
-    ? { rejectUnauthorized: false }
+    ? { rejectUnauthorized: true }
     : false,
 });
 
